e428b56ba2cbbbdb4e6f987b560adad6e8c456c3
AndrYun <yunyatkin95@gmail.com>
2024-06-23 04:13:20 +0300

    Add toggle favorite book

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -18,6 +18,7 @@ const BookForm = () => {
     const randomBookWithId = {
       ...randomBook,
       id: uuidv4(),
+      isFavorite: false,
     };
 
     dispatch(addBook(randomBookWithId));
@@ -31,6 +32,7 @@ const BookForm = () => {
         title,
         author,
         id: uuidv4(),
+        isFavorite: false,
       };
 
       dispatch(addBook(book)); // отправляет действие (action) в магазин
--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,10 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { deleteBook } from '../../redux/books/actionCreators';
+import { BsBookmarkStar, BsBookmarkPlusFill } from 'react-icons/bs';
+import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
 import './BookList.css';
 
 const BookList = () => {
   const books = useSelector((state) => state.books); // подписываемся на часть состояние - массив книг
   const dispatch = useDispatch();
+
+  const toggleBookHandler = (id) => {
+    dispatch(toggleFavorite(id));
+  };
 
   const deleteBookHandler = (id) => {
     dispatch(deleteBook(id));
@@ -22,6 +27,13 @@ const BookList = () => {
                 {++i}. {book.title} by <strong>{book.author}</strong>
               </div>
               <div className="book-actions">
+                <span onClick={() => toggleBookHandler(book.id)}>
+                  {book.isFavorite ? (
+                    <BsBookmarkPlusFill className="star-icon" />
+                  ) : (
+                    <BsBookmarkStar className="star-icon" />
+                  )}
+                </span>
                 <button
                   onClick={() => deleteBookHandler(book.id)}
                   id="deleteBook"
